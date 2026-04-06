5418223557c929f6fdf08bc2f0c6c7518beb92b5
shahidsorrybro-lang <shahidsorrybro@gmail.com>
2026-04-06 18:14:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,4 +5,18 @@ function addToCart(product) {
   alert(product + " added to cart!");
 }function orderNow(product){
   window.open("https://wa.me/919876543210?text=I want to buy " + product);
+}function payNow(){
+  var options = {
+    "key": "rzp_live_SaCvpDZh3fWVVh",
+    "amount": "79900", 
+    "currency": "INR",
+    "name": "Street Mode",
+    "description": "T-Shirt Purchase",
+    "handler": function (response){
+        alert("Payment Successful ✅");
+    }
+  };
+
+  var rzp = new Razorpay(options);
+  rzp.open();
 }
