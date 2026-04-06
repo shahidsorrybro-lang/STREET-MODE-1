78c1be391a90b3ba2e470c8819d5c070aba45d6d
shahidsorrybro-lang <shahidsorrybro@gmail.com>
2026-04-06 17:14:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ let cart = [];
 function addToCart(product) {
   cart.push(product);
   alert(product + " added to cart!");
+}function orderNow(product){
+  window.open("https://wa.me/919876543210?text=I want to buy " + product);
 }
