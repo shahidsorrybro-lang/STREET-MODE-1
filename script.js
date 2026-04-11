18443e9e207d21326db8191f04957e336b802531
shahidsorrybro-lang <shahidsorrybro@gmail.com>
2026-04-11 12:48:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-// =====================
-// CART SYSTEM
-// =====================
-
-// Get cart from storage OR create new
 function getCart(){
   return JSON.parse(localStorage.getItem("cart")) || [];
 }
 
-// Save cart
 function saveCart(cart){
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-// =====================
-// ADD TO CART
-// =====================
 function addToCart(product, price){
   let cart = getCart();
 
@@ -24,55 +15,20 @@ function addToCart(product, price){
   });
 
   saveCart(cart);
-
-  alert(product + " added to cart 🛒");
 }
 
-// =====================
-// BUY WITH SIZE
-// =====================
 function buyProduct(product, price, sizeId){
   let size = document.getElementById(sizeId).value;
 
   if(size === ""){
-    alert("Please select size ❌");
+    alert("Select size ❌");
     return;
   }
 
-  alert(product + " (" + size + ") added to cart 🛒");
-
   addToCart(product + " (" + size + ")", price);
+  alert("Added to cart 🛒");
 }
 
-// =====================
-// VIEW CART PAGE
-// =====================
 function viewCart(){
   window.location.href = "cart.html";
-}
-
-// =====================
-// DIRECT BUY (WHATSAPP)
-// =====================
-function orderNow(product){
-  window.open("https://wa.me/919876543210?text=I want to buy " + product);
-}
-
-// =====================
-// PAYMENT (RAZORPAY)
-// =====================
-function payNow(product, price){
-  var options = {
-    "key": "rzp_live_SaCvpDZh3fWVVh", // your key
-    "amount": price * 100,
-    "currency": "INR",
-    "name": "Street Mode",
-    "description": product,
-    "handler": function (){
-      alert(product + " Payment Successful ✅");
-    }
-  };
-
-  var rzp = new Razorpay(options);
-  rzp.open();
 }
